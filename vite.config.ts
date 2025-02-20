4b7bc78c62418556d364028889184b3d3d5047c9
Mohammed Salah <105545663+MSMelok@users.noreply.github.com>
2025-02-20 08:54:01 +0000

    undoReact

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/stream/' // This is needed for GitHub Pages
-})
